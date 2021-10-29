c60b7348fd08c87ca421041eff2d14388260dd33
Mohsin545-ai <mohsinahmed20017@gmail.com>
2021-10-29 11:11:02 +0530

    first draft of insert

---
+++ insert.sql
@@ -0,0 +1,41 @@
+insert into Product values(1,200,4.5,'lkajdflkjdlkfj','game');
+insert into Product values(1,1000,3.5,'oweirowiur','accessory');
+insert into Product values(2,800,4,'akjdfkljsda','accessory');
+insert into Product values(4,275,4.2,'lkakjdflk','game');
+insert into Product values(2,0,4.8,'oiquweoiu','game');
+insert into Product values(5,350,3,'qoiiweoi','accessory');
+insert into Product values(3,800,3.8,'oiquweoi','game');
+
+insert into product_supplier values(9902144238,'kaljkfdl','cklkad');
+insert into product_supplier values(8723107347,'ahdkfdf','adfkad');
+insert into product_supplier values(7834578245,'akdjflkkasjd','akldjf');
+insert into product_supplier values(9785267547,'alkjlqkj','qiefadj');
+insert into product_supplier values(9902345658,'mvlkadjf','klajdlkfj');
+
+insert into users values('klajdlf','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('lakjdflk','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('alkjdflk','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('lkkaj','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('iouqeoi','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('lkajdlfk','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('lkakjdf','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('mlkjdfl','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('klajdlf','oqiwpei','mohalk@gmail.com',7825780817);
+insert into users values('klajdlf','oqiwpei','mohalk@gmail.com',7825780817);
+
+
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+insert into offers values('ljkadflkjdslfj');
+
+
+
+
+
+
+
